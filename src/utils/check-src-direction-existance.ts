4541ac0700ab77ef8b86a4d6d2416cff0b6848b8
Hossam Sherif <husam287@hotmail.com>
2024-11-16 15:46:58 +0200

    fix src existance check

--- src/utils/check-src-direction-existance.ts
+++ src/utils/check-src-direction-existance.ts
@@ -3,21 +3,13 @@ import * as fs from "fs";
 export default function checkSrcDirectionExistance() {
   const mainDir = process.cwd();
   const allFiles = fs.readdirSync(mainDir, { recursive: true });
-  console.log(allFiles, "DIR all FILES");
 
   const filesFilter = (item: string | Buffer) => {
     if (typeof item !== "string") return false;
 
-    return (
-      !item.includes("node_modules") &&
-      !item.includes("dist") &&
-      item.includes(".mock.json")
-    );
+    return !item.includes("node_modules") && !item.includes("dist");
   };
 
-  
   const filterdFiles = allFiles.filter(filesFilter);
-  console.log(filterdFiles, "DIR filtered FILES");
-  
   return filterdFiles.some((item) => item.includes("src/"));
 }
